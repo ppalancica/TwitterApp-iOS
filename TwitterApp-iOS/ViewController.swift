427c31e932b0365e8ec296fae4f1230ee4298ed5
System Administrator <root@Pavels-MacBook-Pro.local>
2022-02-18 23:12:08 +0200

    First commit. Create a project in Xcode.

---
+++ TwitterApp-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterApp-iOS
+//
+//  Created by Pavel Palancica on 2/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
